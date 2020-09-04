cbd7665d0fbc83e1644d41f7ec8edcfd3101e26f
Dan Wiseman <daniel.wiseman@gmail.com>
2020-09-04 10:07:19 +0000

    refactored the card_symbols to use the regex

--- lib/pretty_magic/icons.rb
+++ lib/pretty_magic/icons.rb
@@ -48,71 +48,30 @@ module PrettyMagic
         saga_number = options[:saga_number]
         
         case symbolText.upcase
-          when'{T}','{TAP}'
-            prettySymbol ='ms-tap'
-          when'{Q}','{UNTAP}'
-            prettySymbol ='ms-untap'
-          when '{TAP-ALT}'
-            prettySymbol ='ms-tap-alt'
-          when'{CHAOS}'
-            prettySymbol ='ms-chaos'
-          when '{ARTIFACT}'
-            prettySymbol = 'ms-artifact'
-          when '{CREATURE}'
-            prettySymbol = 'ms-creature'
-          when '{ENCHANTMENT}'
-            prettySymbol = 'ms-enchantment'
-          when '{INSTANT}'
-            prettySymbol = 'ms-instant'
-          when '{LAND}'
-            prettySymbol = 'ms-land'
-          when'{PW}','{PLANESWALKER}'
-            prettySymbol ='ms-planeswalker'
-          when'{SORCERY}'
-            prettySymbol = 'ms-sorcery'
-          when '{MULTIPLE}'
-            prettySymbol = 'ms-multiple'
-          when '{FLASHBACK}'
-            prettySymbol = 'ms-flashback'
-          when '{LOYALTY-UP}'
-            prettySymbol = ((0...20) === loyalty) ? "ms-loyalty-up ms-loyalty-#{loyalty}" : 'ms-loyalty-up'
-          when '{LOYALTY-DOWN}'
-            prettySymbol = ((0...20) === loyalty) ? "ms-loyalty-down ms-loyalty-#{loyalty}" : 'ms-loyalty-down'
-          when '{LOYALTY-ZERO}'
-            prettySymbol = ((0...20) === loyalty) ? "ms-loyalty-zero ms-loyalty-#{loyalty}" : 'ms-loyalty-zero'
-          when '{LOYALTY-START}'
-            prettySymbol = ((0...20) === loyalty) ? "ms-loyalty-start ms-loyalty-#{loyalty}" : 'ms-loyalty-start'
-          when '{DFC-DAY}'
-            prettySymbol = 'ms-dfc-day'
-          when '{DFC-NIGHT}'
-            prettySymbol = 'ms-dfc-night'
-          when '{DFC-SPARK}'
-            prettySymbol = 'ms-dfc-spark'
-          when '{DFC-IGNITE}'
-            prettySymbol = 'ms-dfc-ignite'
-          when '{DFC-MOON}'
-            prettySymbol = 'ms-dfc-moon'
-          when '{DFC-EMRAKUL}'
-            prettySymbol = 'ms-dfc-emrakul'
-          when '{DFC-ENCHANTMENT}'
-            prettySymbol = 'ms-dfc-enchantment'
-          when '{POWER}'
-            prettySymbol = 'ms-power'
-          when '{TOUGHNESS}'
-            prettySymbol = 'ms-toughness'
-          when '{ARTIST-BRUSH}'
-            prettySymbol = 'ms-artist-brush'
-          when '{ARTIST-NIB}'
-            prettySymbol = 'ms-artist-nib'
-          when '{SAGA}'
-            prettySymbol = ((0...5) === saga_number) ? "ms-saga ms-saga-#{saga_number}" : 'ms-saga'
-          when'{ACORN}'
-            prettySymbol ='ms-acorn'
+          when '{T}'
+            prettySymbol = "ms-tap"
+          when '{Q}'
+            prettySymbol = "ms-untap"
+          when '{PW}'
+            prettySymbol = "ms-planeswalker"
           else
-            return symbolText
+            prettySymbol = ""
         end
         
-        i_tag(prettySymbol + ' ' + optional_classes(options[:cost], options[:shadow], options[:size], options[:fixed_width]))
+        easy_match = symbolText.upcase[/{(TAP|UNTAP|TAP-ALT|CHAOS|ARTIFACT|CREATURE|ENCHANTMENT|INSTANT|LAND|PLANESWALKER|SORCERY|MULTIPLE|FLASHBACK|LOYALTY-UP|LOYALTY-DOWN|LOYALTY-ZERO|LOYALTY-START|DFC-DAY|DFC-NIGHT|DFC-SPARK|DFC-IGNITE|DFC-MOON|DFC-EMRAKUL|DFC-ENCHANTMENT|POWER|TOUGHNESS|ARTIST-BRUSH|ARTIST-NIB|SAGA|ACORN)}/, 1]
+        unless easy_match.nil?
+          prettySymbol = "ms-#{easy_match.downcase}"
+        end
+        
+        if prettySymbol == ""
+          symbolText
+        else
+          prettySymbol = ((0...20) === loyalty) ? prettySymbol + " ms-loyalty-#{loyalty}" : prettySymbol
+          prettySymbol = ((0...5) === saga_number) ? prettySymbol + " ms-saga-#{saga_number}" : prettySymbol
+          
+          i_tag(prettySymbol + ' ' + optional_classes(options[:cost], options[:shadow], options[:size], options[:fixed_width]))
+        end
+        
     end
     
     def self.guild_symbols(symbolText, options = {})
